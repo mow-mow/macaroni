230b956a7d77594e525a42761258a73f5d03dbc6
mow-mow <78189264+mow-mow@users.noreply.github.com>
2021-02-19 12:32:25 +0900

    日付判定ロジック修正

--- src/main/java/info/caprese/macaroni/DateUtil.java
+++ src/main/java/info/caprese/macaroni/DateUtil.java
@@ -6,6 +6,13 @@ import java.time.LocalDateTime;
 
 public class DateUtil {
     public static TimeZone convertTimeZone(LocalDateTime localDateTime) {
-        return TimeZone.MORNING;
+        int hour = localDateTime.getHour();
+        if ( hour >= 5 && hour < 11 ) {
+            return TimeZone.MORNING;
+        } else if ( hour >= 11 && hour < 16 ) {
+            return TimeZone.DAYTIME;
+        } else {
+            return TimeZone.NIGHT;
+        }
     }
 }
